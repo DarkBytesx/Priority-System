59fc7e05e322fac71238701fb102e7dcba08a51a
HairiRaven <152843094+DarkBytesx@users.noreply.github.com>
2024-12-01 13:41:10 +0800

    Update client.lua

--- cat_priority2/client.lua
+++ cat_priority2/client.lua
@@ -23,14 +23,13 @@ end)
 
 RegisterNetEvent('priority:updateNUI')
 AddEventHandler('priority:updateNUI', function(prioType, prioData)
-    local displayMessage = prioData.name  -- Default display message
+    local displayMessage = prioData.name 
 
-    -- Include location in the display message if it's an inprogress robbery
     if prioType == 'inprogress' and prioData.location then
         displayMessage = "-" .. prioData.location
     end
 
-    -- Display different status types with NUI
+
     if prioType == 'safe' or prioType == 'hold' then
         SendNUIMessage({ type = prioData.status, display = prioData.input, name = displayMessage })
     elseif prioType == 'inprogress' then
@@ -39,19 +38,18 @@ AddEventHandler('priority:updateNUI', function(prioType, prioData)
         SendNUIMessage({ type = prioData.status, display = prioData.input, cooldown = prioData.cooldown, name = displayMessage })
     end
 
-    -- Disable robberies if in "hold" or "inprogress"
     ESX.PlayerData.robberiesDisabled = prioType == 'hold' or prioType == 'inprogress' or prioType == 'cooldown'
 end)
 
 RegisterCommand('pdcooldown', function(source, args)
-    local cooldownDuration = tonumber(args[1]) -- Get the cooldown duration from the first argument
+    local cooldownDuration = tonumber(args[1]) 
     if not cooldownDuration then
         TriggerServerEvent('priority:PriorityCD', 15)
         return
     end
     
     if ESX.PlayerData.job.name == 'police' or ESX.PlayerData.job.name == 'admin' then
-        TriggerServerEvent('priority:PriorityCD', cooldownDuration) -- Pass the duration to the server event
+        TriggerServerEvent('priority:PriorityCD', cooldownDuration)
     else
         print('bawal ka!')
     end
@@ -59,18 +57,18 @@ end)
 
 RegisterCommand('pdinprogress', function()
     if ESX.PlayerData.job.name == 'police' or ESX.PlayerData.job.name == 'admin' then
-        -- Use the lib.inputDialog to prompt for the priority name
+    
         local input = lib.inputDialog('Manual In-progress', {'What is Your Latest 10-90? Car Chase / Traphouse / Kidnap / Turfwar?'})
 
-        -- Check if the player entered something
+
         if not input then return end
-        local priorityName = input[1]  -- Get the entered name
+        local priorityName = input[1]
 
         if priorityName and priorityName ~= "" then
-            -- Trigger the server event with the entered name
+
             TriggerServerEvent('priority:PriorityIP', priorityName)
         else
-            -- Notify the player that they need to enter a valid name
+
             TriggerEvent('ox_lib:notify', {
                 id = 'error',
                 title = 'Error',
@@ -81,7 +79,7 @@ RegisterCommand('pdinprogress', function()
             })
         end
     else
-        print('bawal ka!')  -- This is an error message for players who are not in police or ambulance job
+        print('bawal ka!')
     end
 end)
 
@@ -103,23 +101,21 @@ RegisterCommand('pdhold', function()
     end
 end)
 
-local isPrioVisible = false  -- Variable to keep track of the UI visibility
+local isPrioVisible = false
 
 RegisterCommand("pdprio", function()
-    -- Toggle the visibility of the priority UI
     if isPrioVisible then
         SendNUIMessage({
             type = "display",
-            display = true  -- Send message to hide the UI
+            display = true 
         })
     else
         SendNUIMessage({
             type = "display",
-            display = false  -- Send message to display the UI
+            display = false 
         })
     end
 
-    -- Toggle the visibility flag
     isPrioVisible = not isPrioVisible
 end, false)
 
